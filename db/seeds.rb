38a7331692c9e481679b0a7733bdae72fc06e133
aaronfeingold <ajfeingold88@gmail.com>
2021-01-11 17:41:45 -0600

    seeds added for deployment

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,35 +5,35 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-# breakfast = {
-#   name: "El Paso Sunrise",
-#   category: "Breakfast",
-#   description: "Eggs, blackbeans, pico de gallo, cheese Burrito",
-#   vegan: false,
-#   contains_dairy: true,
-#   contains_nuts: false
-# }
+breakfast = {
+  name: "El Paso Sunrise",
+  category: "Breakfast",
+  description: "Eggs, blackbeans, pico de gallo, cheese Burrito",
+  vegan: false,
+  contains_dairy: true,
+  contains_nuts: false
+}
 
-# lunch = {
-#   name: "Provence Salad",
-#   category: "Lunch",
-#   description: "Mixed greens, cherry tomatos, cucumber, artichokes, vegan parm, olives, pistachio, bluberry vinegarette",
-#   vegan: true,
-#   contains_dairy: false,
-#   contains_nuts: true
-# }
+lunch = {
+  name: "Provence Salad",
+  category: "Lunch",
+  description: "Mixed greens, cherry tomatos, cucumber, artichokes, vegan parm, olives, pistachio, bluberry vinegarette",
+  vegan: true,
+  contains_dairy: false,
+  contains_nuts: true
+}
 
-# dinner = {
-#   name: "Big Man Fill Em Up",
-#   category: "Dinner",
-#   description: "Griller steak, potatos, bell peppers, with pasta and sauce",
-#   vegan: false,
-#   contains_dairy: false,
-#   contains_nuts: false
-# }
+dinner = {
+  name: "Big Man Fill Em Up",
+  category: "Dinner",
+  description: "Griller steak, potatos, bell peppers, with pasta and sauce",
+  vegan: false,
+  contains_dairy: false,
+  contains_nuts: false
+}
 
-# Meal.create([
-#   breakfast, 
-#   lunch,
-#   dinner
-# ])
+Meal.create([
+  breakfast, 
+  lunch,
+  dinner
+])
