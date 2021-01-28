55ad0bc2a84490d65efee4ba4d10b9128b752e68
aaronfeingold <ajfeingold88@gmail.com>
2021-01-27 20:37:47 -0600

    cors origins change

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://inspiring-hodgkin-88d8a2.netlify.app'
+    origins 'https://meals-prepped-nola.netlify.app'
 
     resource '*',
       headers: :any,
